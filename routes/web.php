e37635e6b412fefa13476317265b2d5805844abd
adhasan7 <adsansapu@gmail.com>
2024-11-22 13:13:48 +0700

    upload2

--- routes/web.php
+++ routes/web.php
@@ -13,42 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/about', function () {
-    return 9*9;
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/contact', function () {
-    return view('contact');
+
+Route::get('/', function () {
+    return view('home', [
+        'name' => 'Rijal',
+        'role' => 'admin',
+    ]);
 });
 
 
 Route::view('/contact', 'contact', ['name' => 'Taylor','kontak' => '08999' ]);
 
-Route::redirect('/contact', '/contact-us');
-
-Route::get('/product', function (){
-    return "product";
-});
-
-Route::get('/product/{id}', function($id) {
-    return view('product.detail', ['id' => $id]); 
-});
-
-
-
-Route::prefix('saya')->group(function () {
-    Route::get('admin-profile', function() {
-        return "admin profile";
-    });
-    Route::get('admin-about', function() {
-        return "admin about";
-    });
-    Route::get('admin-contact', function() {
-        return "admin contact";
-    });
-});
 
 
